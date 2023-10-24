61f063158adf1b5cee40fa0465d4938167ef3e06
shegi <cristirares@yahoo.com>
2023-10-24 19:42:58 +0300

    Add project files.

---
+++ Data/Sagau_Rares_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sagau_Rares_Lab2.Models;
+
+namespace Sagau_Rares_Lab2.Data
+{
+    public class Sagau_Rares_Lab2Context : DbContext
+    {
+        public Sagau_Rares_Lab2Context (DbContextOptions<Sagau_Rares_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sagau_Rares_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Sagau_Rares_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Sagau_Rares_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Sagau_Rares_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
